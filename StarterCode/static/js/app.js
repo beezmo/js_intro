9e1f22c2dc541a7d4a67818f40ad488bf4433e95
beezmo <37284613+beezmo@users.noreply.github.com>
2019-08-27 12:43:32 -0700

    Added table rows and data with d3

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(sighting) {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
